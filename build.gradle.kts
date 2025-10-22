76c3fda25991a7f946fc7378cc39e85f00d2452e
ruslan.golov <ruslan.golov@jetbrains.com>
2025-10-22 19:11:26 +0200

    all

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,6 @@ dependencies {
   // Playwright Java
   testImplementation("io.qameta.allure:allure-junit5:2.30.0")
 
-//  testImplementation("com.microsoft.playwright:playwright:1.46.0")
-
   // JUnit 5 for tests
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
